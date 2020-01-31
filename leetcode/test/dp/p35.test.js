7090b0d0edb4ccac73c020bf4c91fba9494da463
ObservedObserver <270001151@qq.com>
2020-01-31 21:40:51 +0800

    feat: p35 and test

---
+++ leetcode/test/dp/p35.test.js
@@ -0,0 +1,8 @@
+const maxSubArray = require('../../build/dp/p53.js').maxSubArray;
+const assert = require('assert');
+describe('[p35]', function () {
+  it('example1', function () {
+    const result = maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+    assert.equal(result, 6)
+  })
+})
